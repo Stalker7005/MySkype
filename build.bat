578e3643ee3c2bca360a1df355ac57bc6da0e039
Ivan Yadrov <ivangithab@gmail.com>
2016-06-24 01:33:06 +0300

    added skeleton to future project

---
+++ build.bat
@@ -0,0 +1,11 @@
+MD x86
+cd x86
+cmake .. -G "Visual Studio 14 2015"
+cmake --build . --config Debug
+cd ..
+MD x64
+cd x64
+cmake .. -G "Visual Studio 14 2015 Win64"
+cmake --build . --config Debug
+cd ..
+pause
