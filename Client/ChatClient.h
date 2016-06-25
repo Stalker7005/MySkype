c829fa128c3c370676d8cdf80e030110a3e53b6e
Ivan Yadrov <ivangithab@gmail.com>
2016-06-25 16:53:26 +0300

    added chat message skeleton class skeleton chat client

--- Client/ChatClient.h
+++ Client/ChatClient.h
@@ -6,10 +6,33 @@
 #include <boost/bind.hpp>
 #include <boost/thread/thread.hpp>
 
+#include "ChatMessage.h"
+
 namespace Clients {
 class ChatClient
 {
 public:
-    ChatClient(boost::asio::io_service& io_service);
+    using TIOService = boost::asio::io_service;
+    using TEndPointIter = boost::asio::ip::tcp::resolver::iterator;
+
+public:
+    ChatClient(TIOService& io_service);
+    ~ChatClient();
+
+public:
+    ChatClient(const ChatClient&) = delete;
+    ChatClient& operator=(const ChatClient&) = delete;
+
+public:
+    void Connect(const std::string& host, const std::string& port);
+
+    void PostMessage(const NetworkUtils::ChatMessage& message);
+    NetworkUtils::ChatMessage GetLastMessage();
+
+    void PostMessages(const std::deque<NetworkUtils::ChatMessage>& messages);
+    std::deque<NetworkUtils::ChatMessage> GetMessages();
+
+private:
+
 };
 }
---
+++ NetworkUtilsLib/ChatMessage.h
@@ -0,0 +1,6 @@
+namespace NetworkUtils{
+class ChatMessage
+{
+	
+};
+}
