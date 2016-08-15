122adae6400d31acee2e7feec8a6f1846fad547f
Ivan Yadrov <ivangithab@gmail.com>
2016-08-16 01:20:37 +0300

    added blob to serealization utils lib

--- SerealizationUtilsLib/Blob.cpp
+++ SerealizationUtilsLib/Blob.cpp
@@ -1,15 +1,63 @@
 #include "Blob.h"
 
 namespace Serialization{
+const std::uint64_t Blob::DefaultBlobSize = 64;
 
-Blob::Blob()
+Blob::Blob(): Blob{DefaultBlobSize}
 {
     
 }
 
+Blob::Blob(std::uint64_t size):
+m_blobSize(size)
+{
+    Allocate(size);
+}
+
 Blob::~Blob()
 {
+    Clear();
+}
 
+void Blob::Allocate(std::uint64_t size)
+{
+    if (size > m_blobSize)
+    {
+        m_dataBuf = std::make_unique<std::uint8_t[]>(size);
+        m_blobSize = size;
+    }
+    else
+    {
+        std::memset(m_dataBuf.get(), 0, m_blobSize * sizeof(std::uint8_t));
+    }
+    
+}
+
+std::uint8_t* Blob::GetDataBuf()
+{
+
+}
+
+std::iostream& Blob::GetDataStream()
+{
+    auto& stream = m_dataStream.write(reinterpret_cast<const char*>(m_dataStream.get()), m_blobSize * sizeof(std::uint8_t));
+    if (!stream)
+    {
+        //TODO log error!
+    }
+    return m_dataStream;
+}
+
+std::uint64_t Blob::Size() const
+{
+    return m_blobSize;
+}
+
+void Blob::Clear()
+{
+    m_dataBuf.reset();
+    m_dataStream.clear();
+    m_dataStream.str("");
 }
 
 }
--- SerealizationUtilsLib/Blob.h
+++ SerealizationUtilsLib/Blob.h
@@ -1,18 +1,33 @@
 #pragma once
+#include <memory>
 #include <sstream>
+#include <cstdint>
+#include <cstring>
 
 namespace Serialization{
 class Blob
 {
 public:
     Blob();
+    Blob(std::uint64_t size);
     ~Blob();
 
 public:
+    void Allocate(std::uint64_t size);
+    void Clear();
+    std::uint8_t* GetDataBuf();
+    std::iostream& GetDataStream();
+    std::uint64_t Size() const;
     
 
 private:
+    std::uint64_t m_blobSize;
+    std::unique_ptr<std::uint8_t[]> m_charBuf;
+    std::stringstream m_stream;
     
+
+private:
+    static const std::uint64_t DefaultBlobSize;
 
 };
 }
